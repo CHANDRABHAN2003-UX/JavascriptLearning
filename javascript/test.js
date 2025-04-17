03d2d82375c2989d51eb036209d01a41c75b1fd2
CHANDRABHAN2003-UX <chandrabhansingh57895@gmail.com>
2025-04-17 17:01:44 +0000

    hi

--- javascript/test.js
+++ javascript/test.js
@@ -1,1 +1,19 @@
 console.log("hello golu")
+function test() {
+    let x = 1;
+    if (true) {
+      x = 2;           // Reassigning the same `x`
+      console.log(x);  // 👉 Output: 2
+    }
+    console.log(x);    // 👉 Output: 2
+  }
+  function test() {
+    var x;        // Declaration is hoisted to the top
+    x = 1;        // Initialization
+    if (true) {
+      x = 2;      // Same `x`, just re-assigned
+      console.log(x); // 2
+    }
+    console.log(x); // 2
+  }
+    
---
+++ javascript/variables.js
@@ -0,0 +1,17 @@
+let a="golu"
+let b=Number(a)
+// console.log(a)
+// console.log(b)
+// console.log(typeof b)
+let c=""
+let d=String(c)
+console.log(d)
+console.log(typeof d)
+//data types
+// Int(2 **52)
+// big Int
+// String
+// boolean
+// null standAlonevalue
+// undefined
+// symbol =>used in react component
