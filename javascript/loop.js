314a09a09377cfe1c3154426d973092ef40b5fce
CHANDRABHAN2003-UX <chandrabhansingh57895@gmail.com>
2025-05-03 07:32:49 +0000

    loop

---
+++ javascript/loop.js
@@ -0,0 +1,34 @@
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element)
+    
+// }
+for (let index = 0; index < 10; index++) {
+    for (let index1 = 0; index1 < 10; index1++) {
+        const element1 = index1;
+        console.log(`table of ${index} is ${index}* ${index1} =${index*index1}`)
+        
+    }
+    const element = index;
+    console.log(element)
+    
+}
+let Myarray=['olu','ansu','maa']
+console.log(Myarray.length)
+for (let index = 0; index < Myarray.length; index++) {
+    const element = Myarray[index];
+    console.log(element)
+    
+}
+// while loop //
+let i=0
+while (i<=10) {
+   console.log("value of i is",i) 
+   i++
+}
+let Myarray2=[1,1,2,2,3,3,3,4,4,4,4,]
+let j=0
+while (j<Myarray2.length) {
+    console.log('Xcz',Myarray2[j])
+    j++
+}
